8e0383f85e7c91c0e8c695df6151c51a2f45c509
biomlds <krivushi@ualberta.ca>
2023-03-02 22:55:05 +0000

    add argparse

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Process some integers.")
+parser.add_argument("-a", type=int, help="an integer for the accumulator")
+parser.add_argument("-b", type=int, help="an integer for the accumulator")
+
+args = parser.parse_args()
+
+
 def add_numbers(a, b):
     return a + b
+
+
+if __name__ == "__main__":
+    res1 = add_numbers(args.a, args.b)
+    print(res1)
