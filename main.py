56c4cb543648b2ed440d2eb7ff46bb15f918e385
biomlds <krivushi@ualberta.ca>
2023-03-03 17:36:01 +0000

    test

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import argparse
 parser = argparse.ArgumentParser(description="Process some integers.")
 parser.add_argument("-a", type=int, help="an integer for the accumulator")
 parser.add_argument("-b", type=int, help="an integer for the accumulator")
-
 args = parser.parse_args()
 
 
-def add_numbers(a, b):
-    return a + b
+def add_numbers(alpha, beta):
+    return alpha + beta
+
+
+def multiply_numbers(alpha, beta):
+    return alpha * beta
 
 
 if __name__ == "__main__":
-    res1 = add_numbers(args.a, args.b)
-    print(res1)
+    print("add_numbers: ", add_numbers(args.a, args.b))
+    print("add_numbers: ", multiply_numbers(args.a, args.b))
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,11 @@
-from main import add_numbers
+from main import add_numbers, multiply_numbers
 
 
 def test_add_numbers():
     assert add_numbers(2, 2) == 4
+    assert add_numbers(2, 3) == 5
+
+
+def test_multiply_numbers():
+    assert add_numbers(2, 2) == 4
+    assert multiply_numbers(2, 3) == 6
